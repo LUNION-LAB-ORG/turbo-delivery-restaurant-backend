45b2df578f811b835c90c6badbbb78e757ed038a
wilfriedamoa <wilfriedamoa4@gmail.com>
2024-10-10 21:44:10 +0000

    init push

---
+++ src/main/java/com/lunionlab/turbo_restaurant/TurboRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.lunionlab.turbo_restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurboRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurboRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lunionlab/turbo_restaurant/TurboRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunionlab.turbo_restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurboRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
